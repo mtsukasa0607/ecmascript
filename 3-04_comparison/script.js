f7249cd2ae6b999f3e106558583be831ee9a29d2
mtsukasa0607 <m.tsukasa0607@gmail.com>
2020-12-06 19:00:11 +0900

    入力値と分岐window.alert出力

---
+++ 3-04_comparison/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+const num = Math.floor( Math.random() * 6);
+const my_ans = parseInt(window.prompt('数あてゲーム。0 ~ 5の数字を入力してください。'));
+
+let msg;
+if(my_ans === num) {
+    msg = 'あたり！';
+} else if(my_ans < num) {
+    msg = '残念、もっと大きい';
+} else if(my_ans > num){
+    msg = '残念、もっと小さい';
+} else {
+    msg = '0 ~ 5の数字を入力してください。';
+}
+window.alert(msg);
+
+console.log(num);
+console.log(my_ans);
+console.log(msg);
