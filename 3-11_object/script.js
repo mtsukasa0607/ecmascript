f96efc121285a14e28b518a0f50fcd38aca99391
mtsukasa0607 <m.tsukasa0607@gmail.com>
2020-12-26 14:16:36 +0900

    JSONプロパティメソッド

---
+++ 3-11_object/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let jsbook = {title: 'Javascript', price: 2500, stock: 3};
+console.log(jsbook);
+console.log(jsbook.title);
+console.log(jsbook.price);
+console.log(jsbook.stock);
+console.log(jsbook['price']);
+jsbook.stock = 777;
+console.log(jsbook.stock);
+console.log(jsbook);
+
+let obj = {
+    addTax: function(num) {
+        let tax = 0.1;
+        return num * (1 + tax);
+    }
+}
+console.log(obj);
+console.log(obj.addTax(200));
