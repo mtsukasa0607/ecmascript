de5af421e1a006f3d1f50559a12242ba6a9955c1
mtsukasa0607 <m.tsukasa0607@gmail.com>
2021-01-03 00:58:28 +0900

    Dateオブジェクト年月日秒 ampm表記

--- 4-02_12hour/script.js
+++ 4-02_12hour/script.js
@@ -8,5 +8,14 @@ const hour = now.getHours();
 const min = now.getMinutes();
 const sec = now.getSeconds();
 
+let ampm = '';
+if (hour < 12) {
+    ampm = 'a.m.';
+} else {
+    ampm = 'p.m.';
+}
+
 const output = `${year}/${month + 1}/${date} ${hour}:${min}:${sec}`;
+const output_ampm = `${year}/${month + 1}/${date} ${hour % 12}:${min}:${sec} ${ampm}`;
 document.getElementById('time').textContent = output;
+document.getElementById('time_ampm').textContent = output_ampm;
